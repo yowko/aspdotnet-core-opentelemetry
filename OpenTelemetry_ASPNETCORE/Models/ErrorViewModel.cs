c8e41b198650716cbdbe3d973daf9ee4059e336f
Yowko Tsai <yowko.tsai@skylinetw.com>
2021-03-27 23:02:59 +0800

    repo init

---
+++ OpenTelemetry_ASPNETCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenTelemetry_ASPNETCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
